cf2d333648c7a7492d0b8bd5013853575fb2ed47
leetsworker <you@example.com>
2017-11-02 20:01:40 +0200

    RAILS_ENV=production rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20171026141640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "articles", force: :cascade do |t|
     t.string "ru_title"
     t.text "description"
