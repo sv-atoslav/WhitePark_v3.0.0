4f9f45af7a11a4637885a2bd4f2f38a95f435bac
leetsworker <you@example.com>
2017-11-09 19:29:15 +0200

    simplify dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,30 +1,18 @@
 class Admin::DashboardsController < ApplicationController
-  def dashboard_1
-  end
-
-  def dashboard_2
-  end
-
-  def dashboard_3
-    @extra_class = "sidebar-content"
-  end
-
-  def dashboard_4
-    render :layout => "layout_2"
-  end
-
-  def dashboard_4_1
-  end
-
-  def dashboard_5
-  end
 
   def dashboard_white_park
+    @not_published_opinions = Opinion.where(published: false)
+  end
+
+  def change_defence_info
     if params["new_email"]
       current_moderator.email = params["new_email"]
       current_moderator.save
     end
-    @not_published_opinions = Opinion.where(published: false)
   end
 
+  def log_out
+    sign_out
+    redirect_to root_path
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,44 +8,21 @@ class ApplicationController < ActionController::Base
 	def robots
 	end
 
-	def self.smart_remove_spasebars(tall_string)
-		tall_string.to_s
-		tall_string.gsub!("  "," ") until tall_string.gsub!("  "," ").nil?
-		# I can write
-		# puts " " until tall_string.gsub!("  "," ").nil?
-		# but now f(x) faster
-		tall_string = tall_string[1..-1] while tall_string[0] == " "
-		tall_string = tall_string[0...-1] while tall_string[-1] == " "
-		return tall_string
-	end
-
 	def set_photo_list
 		@photo_list = Photo.all.order(description: :asc)
 	end
 
 	# defense for unauthorized acces
 	def divider_access
-		# puts "controller_name = " + controller_name
-		# puts "action_name = " + action_name
-		puts "params = " + params.to_s
-		info_about_wiever = " "
-		if current_moderator.nil?
-			info_about_wiever += "guest"
-		else
-			info_about_wiever += "moder "
-			info_about_wiever += current_moderator.email
-		end
-		puts "current viever =" + info_about_wiever
-		if moderator_signed_in?
-			if (controller_name == "guest_pages" && action_name == "main" )
-				sign_out
-				# puts "current viever = " + info_about_wiever
-			end
-		else
-			unless (	controller_name == "guest_pages" || legaly_actions.include?(action_name) )
-				authenticate_moderator! 
-			end
-		end
+		# info_about_wiever = " "
+		# if current_moderator.nil?
+		# 	info_about_wiever += "guest"
+		# else
+		# 	info_about_wiever += "moder "
+		# 	info_about_wiever += current_moderator.email
+		# end
+		# puts "current viever =" + info_about_wiever
+		authenticate_moderator! unless moderator_signed_in? || controller_name == "guest_pages" || legaly_actions.include?(action_name)
 	end
 
 	def legaly_actions
@@ -54,13 +31,13 @@ class ApplicationController < ActionController::Base
 
 	private
 
-	def smart_remove_spasebars(tall_string)
+	def self.smart_remove_spasebars(tall_string)
 		tall_string.to_s
 		tall_string.gsub!("  "," ") until tall_string.gsub!("  "," ").nil?
 		# I can write
 		# puts " " until tall_string.gsub!("  "," ").nil?
 		# but now f(x) faster
-		tall_string = tall_string[1..-1] while tall_string[0] == " "
+		tall_string = tall_string[1..-1]  while tall_string[0]  == " "
 		tall_string = tall_string[0...-1] while tall_string[-1] == " "
 		return tall_string
 	end
